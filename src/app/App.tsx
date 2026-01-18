a22d9e1008562ef0ace4a38fe3649d9f9af94faa
Clara <clararemotto@gmail.com>
2026-01-18 09:16:27 -0300

    feat: create and style component button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
-import styles from './app.styles.module.scss';
+import "./app.styles.module.scss";
 
 function App() {
-
   return (
     <>
-      <h1 className={styles['title']}>MindEase</h1>
+      
     </>
-  )
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import '@fontsource/poppins/700.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./app/styles/index.scss"
 import App from '@app/App'
 
 createRoot(document.getElementById('root')!).render(
