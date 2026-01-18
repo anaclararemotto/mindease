db2757e43047ab82c98db0e6c8490d4d32f6c893
Clara <clararemotto@gmail.com>
2026-01-18 14:31:58 -0300

    feat: add dashboard header and shadow variables

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
-import "./app.styles.module.scss";
+import { Button } from "@presentation/components/button/button";
+import styles from"./app.styles.module.scss";
 
 function App() {
   return (
     <>
+    <header className={styles['header']}>
+      <h1 className={styles['header__brand']}>MindEase</h1>
+      <Button label="Login" />
+    </header>
     </>
   );
 }
